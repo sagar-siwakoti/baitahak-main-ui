1627e1d7c14bafca6e40a2bb578b6fc9a0ac09ac
Sagar Siwakoti <siwakotisagar8@gmail.com>
2020-08-27 19:27:09 +0545

    -added sidebar to the ui

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from "react";
 
 import "./App.css";
+import Sidebar from "./components/Sidebar/Sidebar";
 
 function App() {
   return (
     <div className="app">
       <h1>This is a baithak ui</h1>
       {/*Sidebar*/}
+      <Sidebar />
       {/*Feed*/}
-      {/**/}
+      {/* Widgets*/}
     </div>
   );
 }
