3b90a1e160dd1b7a2bfaf8a68b82cc794b7b1866
Sagar Siwakoti <siwakotisagar8@gmail.com>
2020-08-28 15:10:37 +0545

    -added widgets

--- src/components/Widgets/Widgets.jsx
+++ src/components/Widgets/Widgets.jsx
@@ -1,11 +1,30 @@
-import React from 'react';
-import "../../styles/Widgets/Widgets.css"
+import React from "react";
+import "../../styles/Widgets/Widgets.css";
+import {
+  TwitterTimelineEmbed,
+  TwitterTweetEmbed,
+} from "react-twitter-embed";
+import SearchIcon from "@material-ui/icons/Search";
+
 function Widgets(props) {
-    return (
-        <div className="widgets">
-            <h2>Widgets</h2>
-        </div>
-    );
+  return (
+    <div className="widgets">
+      <div className="widgets__input">
+        <SearchIcon className="widgets__searchIcon" />
+        <input placeholder="Search Twitter" type="text" />
+      </div>
+      <div className="widgets__widgetContainer">
+        <h2>What's happening</h2>
+        <TwitterTweetEmbed tweetId={"1296854578726490112"} />
+        <TwitterTimelineEmbed
+          sourceType="profile"
+          screenName="EuropaLeague"
+          options={{ height: 400 }}
+        />
+
+      </div>
+    </div>
+  );
 }
 
 export default Widgets;
